512ba2130a492e6265f79974c5eca48dd95cbe5d
Mohammed Salih k <salihsha656@gmail.com>
2025-06-28 16:58:48 +0530

    second

--- src/pages/SavedJobs.jsx
+++ src/pages/SavedJobs.jsx
@@ -31,7 +31,7 @@ const SavedJobs = () => {
 
       
 
-      <section className="max-w-7xl mx-auto px-6 pb-12">
+      <section className="max-w-7xl mx-auto px-6 pt-10 pb-12">
         <h3 className="text-3xl font-bold text-slate-800 mb-8">Saved Jobs</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
          {Array.isArray(jobs) && jobs.length > 0 ? (
